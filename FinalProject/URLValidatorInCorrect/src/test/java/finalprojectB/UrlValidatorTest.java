21ba143df7034d7dd6b52fff7e0a76b4054b715f
Brooke Weir <weirbr@flip3.engr.oregonstate.edu>
2018-03-17 19:45:28 -0700

    adding manual testing, may add more later

--- FinalProject/URLValidatorInCorrect/src/test/java/finalprojectB/UrlValidatorTest.java
+++ FinalProject/URLValidatorInCorrect/src/test/java/finalprojectB/UrlValidatorTest.java
@@ -22,8 +22,57 @@ public class UrlValidatorTest extends TestCase {
    
    public void testManualTest()
    {
-//You can use this function to implement your manual testing	   
-	   
+       //You can use this function to implement your manual testing
+
+       System.out.println("MTest1: should be valid");
+       UrlValidator urlValidator = new UrlValidator();
+       if (urlValidator.isValid("ftp://foo.bar.com/")) {
+         System.out.println("url is valid");
+       } else {
+         System.out.println("url is invalid");
+       }
+
+      System.out.println("MTest2: should be invalid");
+      String[] schemes = {"http","https"};
+      UrlValidator urlValidator2 = new UrlValidator(schemes);
+      if (urlValidator2.isValid("ftp://foo.bar.com/")) {
+         System.out.println("url is valid");
+      } else {
+         System.out.println("url is invalid");
+      }
+
+       System.out.println("MTest3: should be valid");
+       UrlValidator urlValidator3 = new UrlValidator();
+       if (urlValidator3.isValid("ftp://billgates:moremoney@files.microsoft.com/special/secretplans")) {
+           System.out.println("url is valid");
+       } else {
+           System.out.println("url is invalid");
+       }
+
+       System.out.println("MTest4: should be invalid");
+       UrlValidator urlValidator4 = new UrlValidator(schemes);
+       if (urlValidator4.isValid("ftp://billgates:moremoney@files.microsoft.com/special/secretplans")) {
+           System.out.println("url is valid");
+       } else {
+           System.out.println("url is invalid");
+       }
+
+       System.out.println("MTest5: should be valid");
+       UrlValidator urlValidator5 = new UrlValidator(schemes);
+       if (urlValidator5.isValid("https://google.com")) {
+           System.out.println("url is valid");
+       } else {
+           System.out.println("url is invalid");
+       }
+
+       System.out.println("MTest6: should be valid");
+       UrlValidator urlValidator6 = new UrlValidator(schemes);
+       if (urlValidator6.isValid("https://drive.google.com/")) {
+           System.out.println("url is valid");
+       } else {
+           System.out.println("url is invalid");
+       }
+
    }
    
    
